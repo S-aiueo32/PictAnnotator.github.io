220d967ff4697affc32ff51ab35e0f79f2c3b39d
Sou Uchida <14ec020@ms.dendai.ac.jp>
2017-08-23 18:30:51 +0900

    コード整形

--- scripts.js
+++ scripts.js
@@ -1,13 +1,13 @@
-window.onload=function(){
-    var file_name   =   "日傘の女";
-    var image_block =   document.getElementById('image_block');
-    var image       =   document.getElementById('image');
-    var canvas      =   document.getElementById('canvas');
+window.onload = function() {
+    var file_name = "日傘の女";
+    var image_block = document.getElementById('image_block');
+    var image = document.getElementById('image');
+    var canvas = document.getElementById('canvas');
     //var img_canvas = document.getElementById('img_canvas');
-    var ann_canvas  =   document.getElementById('ann_canvas');
+    var ann_canvas = document.getElementById('ann_canvas');
     //var img_canvas = document.createElement("canvas");
     //var ann_canvas = document.createElement("canvas");
-    
+
     //canvas.width = image.width;
     //canvas.height = image.height;
     //img_canvas.width = image.width;
@@ -16,67 +16,75 @@ window.onload=function(){
     image_block.height = image.height;
     ann_canvas.width = image.width;
     ann_canvas.height = image.height;
-    
+
     //console.log(image_block.height);
-    
+
     //draw_img(img_canvas, image);
-    
-    var data = [[43, 383],[350, 166],[483, 509],[352, 751],[30, 462]];
-    var txt = ["風が吹いている","顔が描かれていない","タッチがよい","色がやわらか","ブルーがきれい"];
+
+    var data = [
+        [43, 383],
+        [350, 166],
+        [483, 509],
+        [352, 751],
+        [30, 462]
+    ];
+    var txt = ["風が吹いている", "顔が描かれていない", "タッチがよい", "色がやわらか", "ブルーがきれい"];
     draw_ann(ann_canvas, data, txt);
-    
+
     //img_canvas.style.display = "inline";
-    
+
     //var ctx = canvas.getContext('2d');
     //ctx.drawImage(img_canvas, 0, 0, canvas.width, canvas.height);
     //ctx.drawImage(ann_canvas, 0, 0, canvas.width, canvas.height);
-    
+
     //img_canvas.style.display = "none";
-    
-    ann_canvas.onclick = function(e){
+
+    ann_canvas.onclick = function(e) {
         var xy = muuXY(e, image_block, this);
         //alert('Xの座標は' + xy[0] + 'Yの座標は' + xy[1]);
-        
+
         var mX = xy[0];
         var mY = xy[1];
-        
+
         document.getElementById('x-coord').innerHTML = mX;
         document.getElementById('y-coord').innerHTML = mY;
-        
+
         var r = mX.toString().concat(",");
         r = r.concat(mY.toString());
         r = r.concat(",");
         r = r.concat(document.getElementById("annotation").value);
         r = r.concat("\n");
         document.getElementById("res").innerHTML = r;
-        
+
         var client_w = ann_canvas.clientWidth;
         var client_h = ann_canvas.clientHeight;
-        
+
         document.getElementById("annotation").value = "Input Tag Text and Click the Tag Position";
         //tweet(r, file_name);
     }
 }
-function tweet(r, name){
+
+function tweet(r, name) {
     var result = r;
     var tag = "&hashtags=TDU_hasegawaLAB_annotation_";
     tag = tag.concat(name);
     //location.href = "https://twitter.com/intent/tweet?text="+ encodeURIComponent(result) +"&hashtags=TDU_hasegawaLAB_annotation";
-    window.open("https://twitter.com/intent/tweet?text="+ encodeURIComponent(result) +tag);
+    window.open("https://twitter.com/intent/tweet?text=" + encodeURIComponent(result) + tag);
 
 }
+
 function muuXY(e, block, that) {
     if (!e) e = window.event;
     var x, y;
     if (e.targetTouches) {
         x = e.targetTouches[0].pageX - e.target.offsetLeft;
         y = e.targetTouches[0].pageY - e.target.offsetTop;
-    }else if (that){
+    } else if (that) {
         x = e.pageX + 1 - (block.offsetLeft + that.offsetLeft);
         y = e.pageY + 1 - (block.offsetTop + that.offsetTop);
         //console.log(that.offsetLeft);
         //console.log(that.offsetTop);
-        
+
         /*
         x = e.pageX - e.target.offsetLeft;
         y = e.pageY - e.target.offsetTop;
@@ -84,7 +92,27 @@ function muuXY(e, block, that) {
         console.log(e.target.offsetTop)
         */
     }
-    return [x,y];
+    return [x, y];
+}
+
+function draw_ann(canvas, data, txt) {
+    var ctx = canvas.getContext('2d');
+    //console.log(data[0][0])
+    for (var i = 0; i < txt.length; i++) {
+        var radius = Math.floor(Math.random() * (30 + 1 - 20)) + 20;
+        ctx.beginPath();
+        ctx.arc(data[i][0], data[i][1], radius, 0, Math.PI * 2, true);
+        var r = Math.floor(Math.random() * 256);
+        var g = Math.floor(Math.random() * 256);
+        var b = Math.floor(Math.random() * 256);
+        ctx.fillStyle = 'rgb(' + r + ',' + g + ',' + b + ')';
+        ctx.globalAlpha = 0.75;
+        ctx.shadowColor = 'rgba(0, 0, 0, 0.5)';
+        ctx.shadowBlur = 3;
+        ctx.shadowOffsetX = 0;
+        ctx.shadowOffsetY = 2;
+        ctx.fill();
+    }
 }
 /*
 function loadCSV(targetFile) {
@@ -119,22 +147,3 @@ function draw_img(canvas, image){
     }
 }
 */
-function draw_ann(canvas, data, txt){
-    var ctx = canvas.getContext('2d');
-    //console.log(data[0][0])
-    for(var i=0; i<txt.length; i++){
-        var radius = Math.floor( Math.random() * (30 + 1 - 20) ) + 20 ;
-        ctx.beginPath();
-        ctx.arc(data[i][0], data[i][1], radius, 0, Math.PI*2, true);
-        var r = Math.floor(Math.random() * 256);
-        var g = Math.floor(Math.random() * 256);
-        var b = Math.floor(Math.random() * 256);
-        ctx.fillStyle = 'rgb(' + r + ',' + g + ',' + b + ')';
-        ctx.globalAlpha = 0.75;
-        ctx.shadowColor = 'rgba(0, 0, 0, 0.5)';
-        ctx.shadowBlur = 3;
-        ctx.shadowOffsetX = 0;
-        ctx.shadowOffsetY = 2;
-        ctx.fill();
-    }
-}
