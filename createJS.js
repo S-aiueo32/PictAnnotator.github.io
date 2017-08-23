acdd8021f3798d46ed2618ba2d36ed5f0d7d35aa
Sou Uchida <14ec020@ms.dendai.ac.jp>
2017-08-24 01:36:30 +0900

    no message

--- createJS.js
+++ createJS.js
@@ -2,6 +2,7 @@ window.addEventListener("load", init);
 
 var cj = createjs,
     circle = new Array(),
+    globalCnt = 0,
     stage,
     img;
 
@@ -19,14 +20,14 @@ function init() {
     circle[0][0].x = stage.canvas.width / 2;
     circle[0][0].y = stage.canvas.height / 2;
     circle[0][0].shadow = new cj.Shadow("rgba(0,0,0,0.25)", 1, 2, 3);
-    circle[0][0].graphics.beginFill("rgba(255,255,255,0.5)").drawCircle(0, 0, 10);
+    circle[0][0].graphics.beginFill("rgba(255,255,255,0.75)").drawCircle(0, 0, 10);
 
 
     circle.push([new cj.Shape, 1]);
     circle[1][0].x = stage.canvas.width / 2 + 20;
     circle[1][0].y = stage.canvas.height / 2 + 20;
     circle[1][0].shadow = new cj.Shadow("rgba(0,0,0,0.25)", 1, 2, 3);
-    circle[1][0].graphics.beginFill("rgba(255,255,255,0.5)").drawCircle(0, 0, 10);
+    circle[1][0].graphics.beginFill("rgba(255,255,255,0.75)").drawCircle(0, 0, 10);
 
     for (var i = 0; i < circle.length; i++)
         stage.addChild(circle[i][0]);
@@ -50,10 +51,11 @@ function handleTick() {
             circle[i][0].scaleX = 1;
             circle[i][0].scaleY = 1;
         }
-        if (circle[i][0].x - 20 < mx && mx < circle[i][0].x + 20)
-            circle[i][0].alpha = 0.75;
+        if ((circle[i][0].x - 15 < mx && mx < circle[i][0].x + 15) &&
+            (circle[i][0].y - 15 < my && my < circle[i][0].y + 15))
+            circle[i][0].alpha = 1;
         else
-            circle[i][0].alpha = 0.5;
+            circle[i][0].alpha = 0.75;
     }
     stage.update();
 }
