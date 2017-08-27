1ec67ca3b1f8202b1260b62d90e008692eedd171
s_aiueo <14ec020@ms.dendai.ac.jp>
2017-08-27 14:57:28 +0900

    マウスオーバでテキスト表示

--- createJS.js
+++ createJS.js
@@ -1,47 +1,35 @@
 window.addEventListener("load", init);
 
 var cj = createjs,
-    circle = new Array(),
+    data,
+    cont_array = new Array(),
     globalCnt = 0,
     stage,
     img,
-    hitObj;
+    hitObj,
+    size;
 
 
 function init() {
+    data = getData();
     stage = new cj.Stage("ann_canvas");
-    var size = getDummySize();
+    img = new cj.Bitmap(getDummyPath());
+    hitObj = new cj.Shape();
+    size = getDummySize();
+
     stage.canvas.width = size[0];
     stage.canvas.height = size[1];
     stage.enableMouseOver();
 
-    img = new cj.Bitmap(getDummyPath());
-    //img.addEventListener("click", handleClick);
-    hitObj = new cj.Shape();
     hitObj.alpha = 0.01;
     hitObj.graphics.beginFill("white").drawRect(0, 0, size[0], size[1]);
     hitObj.addEventListener("click", handleClick);
 
-    circle.push([new cj.Shape, "text", 0]);
-    circle[0][0].x = stage.canvas.width / 2;
-    circle[0][0].y = stage.canvas.height / 2;
-    circle[0][0].shadow = new cj.Shadow("rgba(0,0,0,0.25)", 1, 2, 5);
-    circle[0][0].alpha = 0.5;
-    circle[0][0].graphics.beginFill("white").drawCircle(0, 0, 10);
-
-    circle.push([new cj.Shape, "text", 1]);
-    circle[1][0].x = stage.canvas.width / 2 + 20;
-    circle[1][0].y = stage.canvas.height / 2 + 20;
-    circle[1][0].shadow = new cj.Shadow("rgba(0,0,0,0.25)", 1, 2, 3);
-    circle[1][0].alpha = 0.5;
-    circle[1][0].graphics.beginFill("white").drawCircle(0, 0, 10);
-
     stage.addChild(img);
     stage.addChild(hitObj);
-    for (var i = 0; i < circle.length; i++) {
-        circle[i][0].addEventListener("mouseover", handleMouseOver);
-        circle[i][0].addEventListener("mouseout", handleMouseOut);
-        stage.addChild(circle[i][0]);
+    for (var i = 0; i < data.length; i++) {
+        cont_array.push(makeTag(data[i]));
+        stage.addChild(cont_array[i]);
     }
 
     stage.update();
@@ -51,41 +39,48 @@ function init() {
 cj.Ticker.addEventListener("tick", handleTick);
 cj.Ticker.framerate = 60;
 
+
 function handleTick() {
-    var mx = stage.mouseX,
-        my = stage.mouseY;
-    for (var i = 0; i < circle.length; i++) {
-        circle[i][0].scaleX *= 1.01;
-        circle[i][0].scaleY *= 1.01;
-        if (circle[i][0].scaleX > 2) {
-            circle[i][0].scaleX = 1;
-            circle[i][0].scaleY = 1;
+    for (var i = 0; i < cont_array.length; i++) {
+        circle = cont_array[i].getChildAt(0);
+        circle.scaleX *= 1.01;
+        circle.scaleY *= 1.01;
+        if (circle.scaleX > 2) {
+            circle.scaleX = 1;
+            circle.scaleY = 1;
         }
     }
     stage.update();
 }
 
 function handleMouseOver(e) {
-    e.target.alpha = 0.9;
-    console.log(e.target)
+    var parent = e.target.parent,
+        circle = parent.getChildAt(0),
+        rect = parent.getChildAt(1),
+        text = parent.getChildAt(2);
+    circle.alpha = 0.9;
+    rect.alpha = 1;
+    text.alpha = 1;
 }
 
 function handleMouseOut(e) {
-    e.target.alpha = 0.5;
-    //console.log(e.currentTarget)
+    var parent = e.target.parent,
+        circle = parent.getChildAt(0),
+        rect = parent.getChildAt(1),
+        text = parent.getChildAt(2);
+    circle.alpha = 0.5;
+    rect.alpha = 0;
+    text.alpha = 0;
 }
 
 function handleClick(e) {
     var mx = stage.mouseX,
-        my = stage.mouseY;
-    //console.log(mx, my)
+        my = stage.mouseY,
+        r = mx.toString() + "," + my.toString() + "," +
+        document.getElementById("annotation").value + "\n";
     document.getElementById('x-coord').innerHTML = mx;
     document.getElementById('y-coord').innerHTML = my;
-
-    var r = mx.toString() + "," + my.toString() + "," +
-        document.getElementById("annotation").value + "\n";
     document.getElementById("res").innerHTML = r;
-
     document.getElementById("annotation").value =
         "Input Your Annotation and Click its Position.";
     //tweet(r, file_name);
@@ -99,4 +94,47 @@ function getDummySize() {
 function getDummyPath() {
     var dummy = document.getElementById('dummy');
     return dummy.src;
+}
+
+function getData() {
+    var data = [
+        [47, 227, "風が吹いている"],
+        [224, 86, "顔が描かれていない"],
+        [266, 643, "タッチがよい"]
+    ]
+    return data;
+}
+
+
+function makeTag(data) {
+    var container = new cj.Container(),
+        circle = new cj.Shape(),
+        text = new cj.Text(data[2], "18px Arial", "black"),
+        rect = new cj.Shape();
+
+    circle.x = data[0];
+    circle.y = data[1];
+    circle.alpha = 0.5;
+    circle.shadow = new cj.Shadow("rgba(0,0,0,0.25)", 1, 2, 5);
+    circle.graphics.beginFill("white").drawCircle(0, 0, 10);
+    circle.addEventListener("mouseover", handleMouseOver);
+    circle.addEventListener("mouseout", handleMouseOut);
+    container.addChild(circle);
+
+    text.x = circle.x;
+    text.y = circle.y + 10;
+    text.alpha = 0;
+
+    rect.x = circle.x;
+    rect.y = circle.y + 10;
+    rect.alpha = 0;
+    rect.shadow = new cj.Shadow("rgba(0,0,0,0.25)", 1, 2, 5);
+    rect.graphics.beginFill("white").drawRect(-5, -5,
+        text.getMeasuredWidth() + 10, text.getMeasuredLineHeight() + 10);
+
+    container.addChild(circle);
+    container.addChild(rect);
+    container.addChild(text);
+
+    return container;
 }
